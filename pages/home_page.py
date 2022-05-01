b77f846af450a29925a05a14611583b746331cfd
zyola <ded-undead@mail.ru>
2022-05-01 12:55:09 +0300

    Diplom project 04

--- pages/home_page.py
+++ pages/home_page.py
@@ -2,13 +2,28 @@ from pages.base_page import BasePage
 from selenium.webdriver.common.by import By
 
 
-chapter_catalog_by_header = (By.XPATH, '//*[@id="__layout"]/div/div[1]/header/div[2]/ul/li[1]/a')
+catalog_button_by_header = (By.XPATH, '//*[@id="__layout"]/div/div[1]/header/div[2]/ul/li[1]/a')
 delivery_button_by_header = (By.XPATH, '//*[@id="__layout"]/div/div[1]/header/div[2]/ul/li[2]/a')
 buy_info_button_by_header = (By.XPATH, '//*[@id="__layout"]/div/div[1]/header/div[2]/ul/li[3]/a')
 pay_info_button_by_header = (By.XPATH, '//*[@id="__layout"]/div/div[1]/header/div[2]/ul/li[4]/a')
 contacts_button_by_header = (By.XPATH, '//*[@id="__layout"]/div/div[1]/header/div[2]/ul/li[5]/a')
 cart_button_by_header = (By.CLASS_NAME, 'cart')
 search_button_by_header = (By.XPATH, '//*[@id="__layout"]/div/div[1]/header/div[3]/div[1]/li/a')
+reverting_logo_button_by_header = (By.XPATH, '//*[@id="__layout"]/div/div[1]/header/div[2]/a/div[1]/img')
+
+catalog_button_by_column_menu = (By.XPATH, '//*[@id="__layout"]/div/div[1]/div/div/div[3]/ul/li[1]/a')
+delivery_button_by_column_menu = (By.XPATH, '//*[@id="__layout"]/div/div[1]/div/div/div[3]/ul/li[2]/a')
+buy_info_button_by_column_menu = (By.XPATH, '//*[@id="__layout"]/div/div[1]/div/div/div[3]/ul/li[3]/a')
+articles_button_by_column_menu = (By.XPATH, '//*[@id="__layout"]/div/div[1]/div/div/div[3]/ul/li[4]/a')
+contacts_button_by_column_menu = (By.XPATH, '//*[@id="__layout"]/div/div[1]/div/div/div[3]/ul/li[5]/a')
+
+catalog_button_by_bottom_menu = (By.XPATH, '//*[@id="__layout"]/div/div[3]/div[1]/div[1]/div[2]/ul/li[1]/a')
+information_button_by_bottom_menu = (By.XPATH, '//*[@id="__layout"]/div/div[3]/div[1]/div[1]/div[3]/ul/li[2]/a')
+contacts_button_by_bottom_menu = (By.XPATH, '//*[@id="__layout"]/div/div[3]/div[1]/div[1]/div[3]/ul/li[2]/a')
+delivery_button_by_bottom_menu = (By.XPATH, '//*[@id="__layout"]/div/div[3]/div[1]/div[1]/div[3]/ul/li[3]/a')
+pay_info_button_by_bottom_menu = (By.XPATH, '//*[@id="__layout"]/div/div[3]/div[1]/div[1]/div[3]/ul/li[4]/a')
+articles_button_by_bottom_menu = (By.XPATH, '//*[@id="__layout"]/div/div[3]/div[1]/div[1]/div[3]/ul/li[5]/a')
+privacy_policy_by_bottom_menu = (By.XPATH, '//*[@id="__layout"]/div/div[3]/div[1]/div[1]/div[3]/ul/li[6]/a')
 
 
 class HomePage(BasePage):
@@ -21,7 +36,7 @@ class HomePage(BasePage):
 
     @property
     def open_catalog_by_header(self):
-        return self.find_element(chapter_catalog_by_header)
+        return self.find_element(catalog_button_by_header)
 
     @property
     def open_delivery_by_header(self):
@@ -44,6 +59,57 @@ class HomePage(BasePage):
         return self.find_element(cart_button_by_header)
 
     @property
-    def open_search_header(self):
+    def open_search_by_header(self):
         return self.find_element(search_button_by_header)
 
+    @property
+    def reverting_logo_by_header(self):
+        return self.find_element(reverting_logo_button_by_header)
+
+    @property
+    def open_catalog_by_column_menu(self):
+        return self.find_element(catalog_button_by_column_menu)
+
+    @property
+    def open_delivery_by_column_menu(self):
+        return self.find_element(delivery_button_by_column_menu)
+
+    @property
+    def open_buy_info_by_column_menu(self):
+        return self.find_element(buy_info_button_by_column_menu)
+
+    @property
+    def open_articles_by_column_menu(self):
+        return self.find_element(articles_button_by_column_menu)
+
+    @property
+    def open_contacts_by_column_menu(self):
+        return self.find_element(contacts_button_by_column_menu)
+
+    @property
+    def open_catalog_by_bottom_menu(self):
+        return self.find_element(catalog_button_by_bottom_menu)
+
+    @property
+    def open_information_by_bottom_menu(self):
+        return self.find_element(information_button_by_bottom_menu)
+
+    @property
+    def open_contacts_by_bottom_menu(self):
+        return self.find_element(contacts_button_by_bottom_menu)
+
+    @property
+    def open_delivery_by_bottom_menu(self):
+        return self.find_element(delivery_button_by_bottom_menu)
+
+    @property
+    def open_pay_info_by_bottom_menu(self):
+        return self.find_element(pay_info_button_by_bottom_menu)
+
+    @property
+    def open_articles_by_bottom_menu(self):
+        return self.find_element(articles_button_by_bottom_menu)
+
+    @property
+    def open_privacy_policy_by_bottom_menu(self):
+        return self.find_element(privacy_policy_by_bottom_menu)
---
+++ tests/test_buttons_to_main_pages.py
@@ -0,0 +1,180 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from pages.home_page import HomePage
+from pages.catalog_page import CatalogPage
+from pages.delivery_page import DeliveryPage
+from pages.buy_info_page import BuyInfoPage
+from pages.pay_info_page import PayInfoPage
+from pages.contacts_page import ContactsPage
+from pages.cart_page import CartPage
+
+
+def test_open_catalog_by_header(driver):
+    home_page = HomePage(driver)
+    home_page.open()
+    ActionChains(driver).double_click(home_page.open_catalog_by_header).perform()
+    catalog_page = CatalogPage(driver)
+    assert 'Каталог' in catalog_page.check_catalog_title.text
+
+
+def test_open_delivery_by_header(driver):
+    home_page = HomePage(driver)
+    home_page.open()
+    home_page.open_delivery_by_header.click()
+    delivery_page = DeliveryPage(driver)
+    assert 'Доставка' in delivery_page.check_delivery_title.text
+
+
+def test_open_buy_info_by_header(driver):
+    home_page = HomePage(driver)
+    home_page.open()
+    home_page.open_buy_info_by_header.click()
+    buy_info_page = BuyInfoPage(driver)
+    assert 'Как сделать покупку' in buy_info_page.check_buy_info_title.text
+
+
+def test_open_pay_info_by_header(driver):
+    home_page = HomePage(driver)
+    home_page.open()
+    home_page.open_pay_info_by_header.click()
+    pay_info_page = PayInfoPage(driver)
+    assert 'Способы оплаты' in pay_info_page.check_pay_info_title.text
+
+
+def test_open_contacts_by_header(driver):
+    home_page = HomePage(driver)
+    home_page.open()
+    home_page.open_contacts_by_header.click()
+    contacts_page = ContactsPage(driver)
+    assert 'Контактная информация' in contacts_page.check_contacts_title.text
+
+
+def test_open_cart_by_header(driver):
+    home_page = HomePage(driver)
+    home_page.open()
+    home_page.open_cart_by_header.click()
+    cart_page = CartPage(driver)
+    assert 'Оформление заказа' in cart_page.check_cart_title.text
+
+
+def test_search_by_header(driver):
+    home_page = HomePage(driver)
+    home_page.open()
+    home_page.open_search_by_header.click()
+    search_title = driver.find_element(By.XPATH, '//*[@id="__layout"]/div/div[1]/header/div[4]/div/input')
+    assert '' in search_title.text
+
+
+def test_reverting_logo_by_header(driver):
+    home_page = HomePage(driver)
+    home_page.open()
+    ActionChains(driver).double_click(home_page.open_catalog_by_header).perform()
+    home_page.reverting_logo_by_header.click()
+    unico_image_home_page = driver.find_element(By.XPATH, '//*[@id="__layout"]/div/div[2]/section['
+                                                          '1]/div/div/div/div/div/div/a/div/picture/img')
+    assert unico_image_home_page.is_displayed()
+
+
+# @pytest.mark.skip
+def test_open_catalog_by_column_menu(driver):  # БАГ. Кнопка "Каталог" в столбцовом меню не открывает страницу каталога.
+    home_page = HomePage(driver)
+    home_page.open()
+    home_page.open_catalog_by_header.click()
+    home_page.open_catalog_by_column_menu.click()
+    catalog_page = CatalogPage(driver)
+    assert 'Каталог' in catalog_page.check_catalog_title.text
+
+
+def test_open_delivery_by_column_menu(driver):
+    home_page = HomePage(driver)
+    home_page.open()
+    home_page.open_catalog_by_header.click()
+    home_page.open_delivery_by_column_menu.click()
+    delivery_page = DeliveryPage(driver)
+    assert 'Доставка' in delivery_page.check_delivery_title.text
+
+
+def test_open_buy_info_by_column_menu(driver):
+    home_page = HomePage(driver)
+    home_page.open()
+    home_page.open_catalog_by_header.click()
+    home_page.open_buy_info_by_column_menu.click()
+    buy_info_page = BuyInfoPage(driver)
+    assert 'Как сделать покупку' in buy_info_page.check_buy_info_title.text
+
+
+def test_open_articles_by_column_menu(driver):
+    home_page = HomePage(driver)
+    home_page.open()
+    home_page.open_catalog_by_header.click()
+    home_page.open_articles_by_column_menu.click()
+    articles_title = driver.find_element(By.CLASS_NAME, 'left-title')
+    assert 'Полезные статьи' in articles_title.text
+
+
+def test_open_contacts_by_column_menu(driver):
+    home_page = HomePage(driver)
+    home_page.open()
+    home_page.open_catalog_by_header.click()
+    home_page.open_contacts_by_column_menu.click()
+    contacts_page = ContactsPage(driver)
+    assert 'Контактная информация' in contacts_page.check_contacts_title.text
+
+
+def test_open_catalog_by_bottom_menu(driver):
+    home_page = HomePage(driver)
+    home_page.open()
+    home_page.open_catalog_by_bottom_menu.click()
+    catalog_page = CatalogPage(driver)
+    assert 'Каталог' in catalog_page.check_catalog_title.text
+
+
+# @pytest.mark.skip
+def test_open_information_by_bottom_menu(driver):  # БАГ. Страницы "Информация" не существует. А эта кнопка
+    # направляет на страницу "Доставка".
+    home_page = HomePage(driver)
+    home_page.open()
+    home_page.open_information_by_bottom_menu.click()
+    unico_image_home_page = driver.find_element(By.XPATH, '//*[@id="__layout"]/div/div[2]/section['
+                                                          '1]/div/div/div/div/div/div/a/div/picture/img')
+    assert unico_image_home_page.is_displayed()
+
+
+def test_open_contacts_by_bottom_menu(driver):
+    home_page = HomePage(driver)
+    home_page.open()
+    home_page.open_contacts_by_bottom_menu.click()
+    contacts_page = ContactsPage(driver)
+    assert 'Контактная информация' in contacts_page.check_contacts_title.text
+
+
+def test_open_delivery_by_bottom_menu(driver):
+    home_page = HomePage(driver)
+    home_page.open()
+    home_page.open_delivery_by_bottom_menu.click()
+    delivery_page = DeliveryPage(driver)
+    assert 'Доставка' in delivery_page.check_delivery_title.text
+
+
+def test_open_pay_info_by_bottom_menu(driver):
+    home_page = HomePage(driver)
+    home_page.open()
+    home_page.open_pay_info_by_bottom_menu.click()
+    pay_info_page = PayInfoPage(driver)
+    assert 'Способы оплаты' in pay_info_page.check_pay_info_title.text
+
+
+def test_open_articles_by_bottom_menu(driver):
+    home_page = HomePage(driver)
+    home_page.open()
+    home_page.open_articles_by_bottom_menu.click()
+    articles_title = driver.find_element(By.CLASS_NAME, 'left-title')
+    assert 'Полезные статьи' in articles_title.text
+
+
+def test_open_privacy_policy_by_bottom_menu(driver):
+    home_page = HomePage(driver)
+    home_page.open()
+    home_page.open_privacy_policy_by_bottom_menu.click()
+    privacy_policy_title = driver.find_element(By.CLASS_NAME, 'left-title')
+    assert 'Политика конфиденциальности' in privacy_policy_title.text
